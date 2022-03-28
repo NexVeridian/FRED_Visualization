7e06d86839e1a42a772ebc8a2a106d2af66e2aa2
NexVeridian <NexVeridian@gmail.com>
2022-03-27 22:25:29 -0700

    1.0

---
+++ FRED_Bokeh.py
@@ -0,0 +1,76 @@
+from datetime import date
+from itertools import cycle
+
+import bokeh
+import pandas as pd
+from bokeh.io import curdoc, show
+from bokeh.layouts import column, grid
+from bokeh.models import DateRangeSlider, tools, Span
+from bokeh.models.sources import ColumnDataSource
+from bokeh.palettes import Dark2_8 as palette
+from bokeh.plotting import figure
+
+
+def chartFRED(df: pd.DataFrame) -> bokeh.layouts.layout:
+    curdoc().theme = "dark_minimal"
+    DateMinMax = (df["Date"].min(), df["Date"].max())
+    DataCols = df.columns.drop(["Date"]).tolist()
+
+    Date_Range_Slider = DateRangeSlider(
+        start=pd.to_datetime(df["Date"].min()),
+        end=pd.to_datetime(date.today()),
+        step=(1 * 24 * 60 * 60 * 1000) * 30,  # 30 days in milliseconds
+        value=DateMinMax,
+    )
+    source = ColumnDataSource(df)
+
+    p1 = figure(
+        x_range=DateMinMax,
+        x_axis_type="datetime",
+        toolbar_location=None,
+        active_drag=None,
+        active_scroll=None,
+        active_tap=None,
+        output_backend="webgl",
+    )
+
+    colors = cycle(palette)
+    c = next(colors)
+    plot1a = p1.line("Date", DataCols[0], source=source, legend_label=DataCols[0], color=c)
+    for i, c in zip(range(1, len(DataCols)), colors):
+        p1.line("Date", DataCols[i], source=source, legend_label=DataCols[i], color=c)
+
+    Date_Range_Slider.js_link("value", p1.x_range, "start", attr_selector=0)
+    Date_Range_Slider.js_link("value", p1.x_range, "end", attr_selector=1)
+    p1.legend.location = "top_left"
+    p1.legend.orientation = "horizontal"
+
+    TooltipGuide = [(x, f"@{x}" + "{0.0a}") for x in DataCols]
+    Format = {"@Date": "datetime"}
+    Format.update({f"@{x}": "numeral" for x in DataCols})
+
+    p1.renderers.extend([Span(location=0, dimension="width", line_color="gray", line_dash="dashed", line_width=3)])
+
+    hover = tools.HoverTool(
+        renderers=[plot1a],
+        tooltips=[
+            ("Date", "@Date{%F}"),
+            *TooltipGuide,
+        ],
+        formatters=Format,
+        mode="vline",
+    )
+
+    p1.add_tools(hover)
+
+    layout = grid(
+        [
+            column(
+                [Date_Range_Slider],
+                sizing_mode="stretch_width",
+            ),
+            [p1],
+        ],
+        sizing_mode="stretch_both",
+    )
+    return layout
---
+++ README.md
@@ -0,0 +1,6 @@
+Bokeh charting example using Federal reserve economic data [FRED](https://fred.stlouisfed.org/)
+For CPI, Core CPI, and Stick CPI
+
+For a live example run: Bokeh_Example.ipynb or use [![Binder](https://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/NexVeridian/FRED_Visualization/main?labpath=Bokeh_Example.ipynb)
+
+![chartFRED](https://github.com/NexVeridian/FRED_Visualization/blob/main/chartFRED.png?raw=true)
